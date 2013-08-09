c2c78391760103b43a322882a8088844748b458e
Pablo Orduña <pablo.orduna@deusto.es>
2013-08-09 02:22:26 +0200

    Use templates

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
+import os
 from staticjinja import Renderer
 
 
 if __name__ == "__main__":
-    renderer = Renderer(outpath='./output')
+    def filter_func(filepath):
+        filename = os.path.basename(filepath)
+        if filename.startswith(('.', '_')):
+            return False
+        if not filename.endswith('.html'):
+            return False
+        return True
+
+    def dont_render(*args, **kwargs):
+        print "Rendering..."
+        pass
+
+    renderer = Renderer(outpath='./output', rules = [ ('.*.swp', dont_render) ])
+    renderer.filter_func = filter_func
     renderer.run(debug=True, use_reloader=True)
