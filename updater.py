ec22f8c5b46a13029dbd5c5fbc040284b0af77b0
Pablo Orduña <pablo.orduna@deusto.es>
2013-10-09 18:37:22 +0200

    Update the updater

--- updater.py
+++ updater.py
@@ -4,21 +4,30 @@ import sys
 import time
 import os
 import subprocess
+import traceback
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# Don't execute a script from github so easily. Manually restart this process for that.
+run_code = open("run.py").read()
+open('/tmp/run.py', 'w').write(run_code)
 
 current_version = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
 
 while True:
+    time.sleep(30)
+    sys.stderr.flush()
     print "Checking...", time.asctime()
     sys.stdout.flush()
-    subprocess.check_output(['git', 'pull', 'origin', 'master'])
-    new_version = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-    if new_version != current_version:
-        print "Change detected (%s -> %s). Compiling" % (current_version, new_version),time.asctime()
-        sys.stdout.flush()
-        current_version = new_version
-        subprocess.Popen([sys.executable, 'run.py', '--dont-stop'])
+    try:
+        subprocess.check_output(['git', 'pull', 'origin', 'master'])
+        new_version = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+        if new_version != current_version:
+            print "Change detected (%s -> %s). Compiling" % (current_version, new_version),time.asctime()
+            sys.stdout.flush()
+            current_version = new_version
+            subprocess.Popen([sys.executable, '/tmp/run.py', '--dont-stop'])
+    except:
+        traceback.print_exc()
+        continue
 
-    time.sleep(30)
