3f9f93f42856f8934086eba95fdb8133a02df8b2
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-10-17 12:31:53 +0200

    Added Markdown support as a Jinja2 filter

--- run.py
+++ run.py
@@ -1,7 +1,25 @@
 import os
+from jinja2 import Environment
+
+import markdown
+import mdx_smartypants
 
 from staticjinja import Renderer
 from optparse import OptionParser
+
+
+
+def markdown_filter(source):
+    """
+    Markdown filter for Jinja2.
+    :param source: Source in markdown format
+    :return: Markdown rendered HTML.
+    """
+    return markdown.markdown(source, extensions=[mdx_smartypants.makeExtension({"entities":"named"})])
+
+def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
+    return value.strftime(format)
+
 
 if __name__ == "__main__":
     parser = OptionParser()
@@ -22,5 +40,9 @@ if __name__ == "__main__":
         pass
 
     renderer = Renderer(outpath='./output', rules = [ ('.*.tmp', dont_render), ('.*.swx', dont_render) ])
+    renderer._env.filters["markdown"] = markdown_filter
+    renderer._env.filters['datetimeformat'] = datetimeformat
     renderer.filter_func = filter_func
+
+
     renderer.run(debug=True, use_reloader = not options.dont_stop)
